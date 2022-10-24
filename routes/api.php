20ede1d268d9965e04be4c9315257b5a71035139
RhinolamerSonwar <rhinolamer@gmail.com>
2022-10-25 00:17:17 +0300

    *Added post routes

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::post('post/create', 'PostController@store');
+Route::post('post/{post}/update', 'PostController@update');
+Route::get('post/{post}', 'PostController@show');
+Route::delete('post/{post}', 'PostController@destroy');
+Route::get('posts', 'PostController@index');
+
+
